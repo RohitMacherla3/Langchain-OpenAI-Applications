b78a2673af452c6409e94d3f9d02c2157f64efab
Rohit Macherla <rm1667@scarletmail.rutgers.edu>
2024-04-29 01:13:41 -0400

    deployment update

--- pdf_chat_app.py
+++ pdf_chat_app.py
@@ -7,25 +7,29 @@ from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
 
 # vector database
-import cassio
-from langchain_community.vectorstores import Cassandra
+from langchain_community.vectorstores import FAISS
 
 # chatmodel
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import RetrievalQA
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
-
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage
+from htmlTemplates import css, bot_template, user_template
 
 # read pdf into a single text and splict into chunks
 def get_text_chunks(pdfs):
+    
     text = ""
-    for pdf in pdfs:
-        pdf_reader = PdfReader(pdf)
+    if pdfs == 'docs/attention.pdf':
+        pdf_reader = PdfReader(pdfs)
         for page in pdf_reader.pages:
             text +=page.extract_text()
+    else:
+        for pdf in pdfs:
+            pdf_reader = PdfReader(pdf)
+            for page in pdf_reader.pages:
+                text +=page.extract_text()
     
     text_splitter = CharacterTextSplitter(
         separator = "\n",
@@ -38,43 +42,77 @@ def get_text_chunks(pdfs):
     return chunks
 
 def get_conversation_chain(vectorstore):
-    memory = ConversationBufferMemory(memory_key='chat_history', return_memory=True, input_key='question')
-
-    conversation_chain = RetrievalQA.from_chain_type(
-        llm = ChatOpenAI(api_key=OPENAI_API_KEY),
-        chain_type = "stuff",
-        retriever = vectorstore.as_retriever(),
-        chain_type_kwargs = {"memory": memory}
+    llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name='gpt-3.5-turbo', temperature=0.1)
+    retriever = vectorstore.as_retriever()
+    
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, just "
+        "reformulate it if needed and otherwise return it as is."
     )
     
-    return conversation_chain
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+    
+    qa_system_prompt = (
+        "You are an assistant for question-answering tasks. Use "
+        "the following pieces of retrieved context to answer the "
+        "question. If you don't know the answer, just say that you "
+        "don't know. Use three sentences maximum and keep the answer "
+        "concise."
+        "{context}"
+    ) 
+    
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [("system", qa_system_prompt), MessagesPlaceholder("chat_history"), ("human", "{input}"),]
+    )
+
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt) 
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    return rag_chain
 
 
 def get_output_response(question):
-    response = st.session_state.conversation.run(question)
-    st.write(response)
-    # st.session_state.chat_history = response['chat_history']
+    response = st.session_state.conversation.invoke({"input": question, "chat_history": st.session_state.chat_history})
+    st.session_state.chat_history.extend([HumanMessage(content=question), response["answer"]])
         
-    # for i, message in enumerate(st.session_state.chat_history):
-    #     if i % 2 == 0:
-    #         st.write('Hello, here is your answer: \n', message.content)
-    #     else:
-    #         st.write(message.content)
+    for i, message in enumerate(st.session_state.chat_history):
+        if i %2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", str(message)), unsafe_allow_html=True)
     
 def main():
     
     st.set_page_config(page_title = "Chat with PDFs")
     st.header('Chat with PDFs')
+    st.write(css, unsafe_allow_html=True)
     
     if 'conversation' not in st.session_state:
         st.session_state.conversation = None
         
     if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = None
+        st.session_state.chat_history = []
     
     with st.sidebar:
         st.subheader('Your documents')
-        input_pdfs = st.file_uploader('Upload your PDFs here and click Process', type='pdf', accept_multiple_files=True)
+        model_type = st.sidebar.radio("Select File", ['Use pdf of Attention is all you need paper', 'Upload pdf'])
+        if model_type == 'Use pdf of Attention is all you need paper':
+            input_pdfs = 'docs/attention.pdf'
+        elif model_type == 'Upload pdf':
+            input_pdfs = st.file_uploader('Upload your PDFs here and click Process', type='pdf', accept_multiple_files=True)
         
         if st.button('Process'):
             if len(input_pdfs) > 0:
@@ -84,47 +122,34 @@ def main():
                     # get text chunks from pdfs
                     text_chunks = get_text_chunks(input_pdfs)
                     
-                    # loading FAISS vector store with dynamic table name
-                    pdf_name =""
-                    for pdf in input_pdfs:
-                        pdf_name += pdf.name[:-4]
-                        
-                    table_name_dynamic = 'pdf_query_' + str(pdf_name)
-        
+                    # convert text to embeddings
                     embeds = OpenAIEmbeddings(model='text-embedding-3-small')
-                        
-                    cassio.init(token=ASTRA_DB_APPLICATION_TOKEN, database_id=ASTRA_DB_API_ENDPOINT)
-                    vectorstore = Cassandra(
-                        embedding=embeds,
-                        table_name=table_name_dynamic,
-                        session = None,
-                        keyspace = None
-                    )
-                        
-                    vectorstore.add_texts(text_chunks)
+                    
+                    # store the embeddings into a vectore store
+                    vectorstore = FAISS.from_texts(text_chunks, embedding=embeds)
                     
                     st.write("Files Processed!")
                     
+                    
                     st.session_state.conversation = get_conversation_chain(vectorstore)
-                    # output_response = get_response_pdf(input_file, question, domain)
                     
             else:
                 st.write("Please upload a file to process")
     
-    # domain = st.text_input('Choose a domain for the document(s): ')
     question = st.text_input('Ask questions about your document(s): ')
     click = st.button('Generate')
     
     if question and click:
-        with st.spinner('Generating...'):
-            get_output_response(question)
+        if st.session_state.conversation is not None:
+            with st.spinner('Generating...'):
+                get_output_response(question)
+        else:
+            st.write('Please upload a file to process')
 
 
 if __name__ == "__main__":
     
     #initializing all secret keys
     load_dotenv()
-    ASTRA_DB_API_ENDPOINT = os.getenv('ASTRA_DB_API_ENDPOINT')
-    ASTRA_DB_APPLICATION_TOKEN = os.getenv('ASTRA_DB_APPLICATION_TOKEN')
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')    
     main()
